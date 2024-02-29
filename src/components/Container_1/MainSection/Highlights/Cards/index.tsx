7b44851302b32285a1e96ef365c7b6e127481cc6
chouzzy <mathfernando@hotmail.com>
2024-02-29 10:13:30 -0300

    call to action button

--- src/components/Container_1/MainSection/Highlights/Cards/index.tsx
+++ src/components/Container_1/MainSection/Highlights/Cards/index.tsx
@@ -1,15 +1,36 @@
-import { Flex, Image, Text } from "@chakra-ui/react";
+import { Flex, Image, Text, useBreakpointValue } from "@chakra-ui/react";
 import { RoadHorizon, Heart, Horse } from "@phosphor-icons/react";
 import { AirplaneTakeoff } from "phosphor-react";
 import { Card_1 } from "./Card_1";
 import { Card_2 } from "./Card_2";
+import { CallToActionButton } from "../../../../CallToActionButton";
 
 export function Cards() {
+
+    const isMobileVersion = useBreakpointValue({
+        base:true,
+        sm:true,
+        md:true,
+        lg:false,
+        xl:false
+    })
 
     return (
 
         <>
             <Card_1 />
+            {isMobileVersion?
+            <CallToActionButton
+                bgCircleColor="teal.300"
+                bgColor="dark.800"
+                fontColor="light.400"
+                height="2.5rem"
+                width='15rem'
+                title="Adquirir minha cidadania"
+            />
+            :
+            ''
+            }
             <Card_2 />
         </>
     )
