99bbab2b1d524c34e650ef726b1e719aae39df62
chouzzy <mathfernando@hotmail.com>
2024-04-08 14:58:26 -0300

    tag manager

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -36,9 +36,9 @@ function MyApp({ Component, pageProps }: AppProps) {
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-TGLDLST');
+            })(window,document,'script','dataLayer','GTM-M7SF42TR');
          */}
-         <Script async src="https://www.googletagmanager.com/gtag/js?id=GTM-TGLDLST"></Script>
+         <Script async src="https://www.googletagmanager.com/gtag/js?id=GTM-M7SF42TR"></Script>
 
          <Script id="google-tag-manager" strategy="afterInteractive" dangerouslySetInnerHTML={{
             __html: `           
@@ -46,7 +46,7 @@ function MyApp({ Component, pageProps }: AppProps) {
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
 
-            gtag('config', 'GTM-TGLDLST');`
+            gtag('config', 'GTM-M7SF42TR');`
          }}>
          </Script>
 
