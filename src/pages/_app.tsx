206ecb594da1d3a61ff44ef65727d107b76cd112
chouzzy <mathfernando@hotmail.com>
2024-04-16 11:00:39 -0300

    G-V5XTNB47ME

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -36,9 +36,9 @@ function MyApp({ Component, pageProps }: AppProps) {
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-5H6LW6R2');
+            })(window,document,'script','dataLayer','G-V5XTNB47ME');
          */}
-         <Script async src="https://www.googletagmanager.com/gtag/js?id=GTM-5H6LW6R2"></Script>
+         <Script async src="https://www.googletagmanager.com/gtag/js?id=G-V5XTNB47ME"></Script>
 
          <Script id="google-tag-manager" strategy="afterInteractive" dangerouslySetInnerHTML={{
             __html: `           
@@ -46,7 +46,7 @@ function MyApp({ Component, pageProps }: AppProps) {
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
 
-            gtag('config', 'GTM-5H6LW6R2');`
+            gtag('config', 'G-V5XTNB47ME');`
          }}>
          </Script>
 
