137ff26be93ca1619eb595915c2dceee87eb744d
chouzzy <mathfernando@hotmail.com>
2024-04-17 13:39:29 -0300

    G-V5XTNB47ME com o antigo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -41,13 +41,14 @@ function MyApp({ Component, pageProps }: AppProps) {
          <Script async src="https://www.googletagmanager.com/gtag/js?id=G-V5XTNB47ME"></Script>
 
          <Script id="google-tag-manager" strategy="afterInteractive" dangerouslySetInnerHTML={{
-            __html: `           
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'G-V5XTNB47ME');`
-         }}>
+            __html:
+               `
+         (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+         })(window,document,'script','dataLayer','G-V5XTNB47ME');
+         `}}>
          </Script>
 
          {/* <!-- End Google Tag Manager --> */}
