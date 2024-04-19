43e4fcf897f0527fd8b724fd7f2d75abfc57476d
chouzzy <mathfernando@hotmail.com>
2024-04-19 14:55:26 -0300

    AW-327364871

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -41,9 +41,9 @@ function MyApp({ Component, pageProps }: AppProps) {
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-TGLDLST');
+            })(window,document,'script','dataLayer','AW-327364871');
          */}
-         <Script async src="https://www.googletagmanager.com/gtag/js?id=GTM-TGLDLST"></Script>
+         <Script async src="https://www.googletagmanager.com/gtag/js?id=AW-327364871"></Script>
 
          <Script id="google-tag-manager" strategy="afterInteractive" dangerouslySetInnerHTML={{
             __html:
@@ -52,7 +52,7 @@ function MyApp({ Component, pageProps }: AppProps) {
          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-         })(window,document,'script','dataLayer','GTM-TGLDLST');
+         })(window,document,'script','dataLayer','AW-327364871');
          `}}>
          </Script>
 
