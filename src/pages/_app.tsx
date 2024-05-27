a5dc75eb30bcb816bb4024b55e9ba91307c18c45
chouzzy <mathfernando@hotmail.com>
2024-05-27 12:37:54 -0300

    scripts b242ga

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -34,15 +34,6 @@ function MyApp({ Component, pageProps }: AppProps) {
    }, [router.events])
    return (
       <>
-         {/* <!-- Meta Facebook --> */}
-         {/* <!-- Google Tag Manager --> */}
-         {/*
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','G-V5XTNB47ME');
-         */}
          <Script async src="https://www.googletagmanager.com/gtag/js?id=G-V5XTNB47ME"></Script>
 
          <Script id="google-tag-manager" strategy="afterInteractive" dangerouslySetInnerHTML={{
@@ -55,30 +46,44 @@ function MyApp({ Component, pageProps }: AppProps) {
          })(window,document,'script','dataLayer','G-V5XTNB47ME');
          `}}>
          </Script>
-         <Script id="google-tag-manager" strategy="afterInteractive" dangerouslySetInnerHTML={{
-            __html:
-               `
-               <!-- B242GA start -->
-
+         <Script id="B242GA-tag-scrpits" strategy="afterInteractive" dangerouslySetInnerHTML={{
+            __html: `
                (function(w,d,u){
-                  var s=d.createElement('script');s.defer=false;s.async=false;s.id='b242ga-script';s.src=u+'?'+(Date.now()/60000|0);
-                  var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
+               var s=d.createElement('script');s.async=true;s.defer=true;s.src=u+'?'+(Date.now()/60000|0);
+               var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
                })(window,document,'https://67p.b242ga.ru/static/js/b242ga.js');
-               var b242gaScript = document.querySelector('#b242ga-script');
-               b242gaScript.addEventListener('load', function() {
+               
+               // Certifique-se de que o script B242GA está carregado antes de chamar B242GAInit
+               function checkAndInitB242GA() {
+               if (typeof B242GAInit === 'function') {
                   B242GAInit({
                      portal:'https://clubedopassaporte.bitrix24.com.br/',
                      pid:'b4b741b89d40f823c6e4c3ca25cd6982',
                      presets:{}
-                  }); 
-               });
-
-               <!-- B242GA end -->
-               
-         `}
-
-         }>
+                  });
+               } else {
+                  setTimeout(checkAndInitB242GA, 100); // Verificar novamente após 100ms
+               }
+               }
+               checkAndInitB242GA(); 
+    `,
+         }}>
          </Script>
+
+         <Script
+            id="bitrix24-form" // Adicione um ID para facilitar a identificação e gerenciamento
+            strategy="afterInteractive" // Garante que o script seja executado após a renderização inicial do conteúdo
+            dangerouslySetInnerHTML={{
+               __html: `
+        (function(w,d,u){
+          var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/180000|0);
+          var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
+        })(window,document,'https://cdn.bitrix24.com.br/b27154285/crm/form/loader_109.js');
+      `,
+            }}
+            data-b24-form="inline/109/pac0ng" // Mantém os atributos originais
+            data-skip-moving="true"
+         />
 
          {/* <!-- End Google Tag Manager --> */}
 
